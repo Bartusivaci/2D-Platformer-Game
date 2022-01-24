b948c1658a9292fad46e710eb0560746f7e8ea84
Bartusivaci <bartusivaci@gmail.com>
2022-01-24 07:38:11 +0300

    Update PlayerMovement.cs

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] float runSpeed = 10f;
+
+    Vector2 moveInput;
+    Rigidbody2D playerRigid;
+
     void Start()
     {
-        
+        playerRigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed , playerRigid.velocity.y);
+        playerRigid.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigid.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigid.velocity.x), 1f);
+        }
         
     }
 }
